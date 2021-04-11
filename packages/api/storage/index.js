df443a49cf92d21a0875081bce86f409b20300fd
SniperFox213 <dj.gleb@list.ru>
2021-04-11 11:35:48 +0300

    🔨 fix: meh, we were going to change our database locations anyway

--- packages/api/storage/index.js
+++ packages/api/storage/index.js
@@ -4,9 +4,8 @@ const { MongoClient } = require("mongodb");
 // Exporting our function
 const handler = (callback) => {
   // Configuration
-  // process.env.STORAGE_URL
-  const url     = "mongodb://DoggyStyle:uvjfSAb%40iVbC2k@node.paw.unfull.ml:27017/?authSource=pawcapsu";
-  const dbName  = "pawcapsu";
+  const url     = process.env.STORAGE_URL;
+  const dbName  = "dormcloud";
 
   // Connecting to our datastore
   MongoClient.connect(url, (error, client) => {
